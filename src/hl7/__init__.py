d30ef2696635b81f4fc4ea23046a2be316824852
Luke Leighton <lkcl@lkcl.net>
2010-08-06 14:37:02 +0100

    code shuffle

---
+++ src/hl7/__init__.py
@@ -0,0 +1,12 @@
+from hl7 import *
+
+__version__ = '0.1.1~xml.2'
+__author__ = 'John Paulett; Luke Leighton'
+__email__ = 'john -at- 7oars.com; lkcl@lkcl.net'
+__license__ = 'BSD'
+__copyright__ = """Copyright 2009, John Paulett <john -at- 7oars.com>
+Copyright (C) 2010, Luke Kenneth Casson Leighton <lkcl@lkcl.net>"""
+# oops, has to be http://github.com/lkcl/hl7 for now until john merges/maintains
+#__url__ = 'http://www.bitbucket.org/johnpaulett/python-hl7/wiki/Home'
+__url__ = 'http://github.com/lkcl/hl7'
+
