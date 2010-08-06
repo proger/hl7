1506f0ab6b082a047640986a7f2d51a90c956b57
Luke Leighton <lkcl@lkcl.net>
2010-08-06 16:07:21 +0100

    update

---
+++ src/hl7/segments.py
@@ -0,0 +1,15 @@
+import segments21
+import segments22
+import segments23
+import segments231
+import segments24
+import segments25
+
+segment_revs = {'2.1': segments21,
+                    '2.2': segments22,
+                    '2.3': segments23,
+                    '2.31': segments231,
+                    '2.4': segments24,
+                    '2.5': segments25,
+                   }
+
